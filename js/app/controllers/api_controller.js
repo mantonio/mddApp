a75d690196b774b4eaf353c5abd17404dbbfc4da
Matt Antonio <mantonio@fullsail.edu>
2014-04-17 20:19:56 -0400

    setting up google+ login. Created MongoDB to store user data

--- js/app/controllers/api_controller.js
+++ js/app/controllers/api_controller.js
@@ -2,7 +2,7 @@ app.controller("ApiController", function($scope, $http) {
     $scope.googleMapsKey = "AIzaSyB0w2_dnlKE13DikhEpEO4MBUb7-ZtGFn4";
 
     var gMapsInit = function() {
-        var defaultLatLng = new google.maps.LatLng(28.597161,-81.301508);
+        var defaultLatLng = new google.maps.LatLng(28.597161, -81.301508);
         var mapOptions = {
             center: defaultLatLng,
             zoom: 17
--- js/app/main.js
+++ js/app/main.js
@@ -1,9 +1,13 @@
-var app = angular.module('mddFSApp', ['ngRoute']);
+var app = angular.module('mddFSApp', ['ngRoute', 'directive.g+signin']);
 
 app.config(function($routeProvider) {
     $routeProvider
         .when('/', {
             templateUrl: 'views/list_view.html',
             controller: 'ApiController'
+        })
+        .when('/addFood', {
+            templateUrl: 'views/addFood_view.html',
+            controller: 'AddFoodController'
         });
 });
