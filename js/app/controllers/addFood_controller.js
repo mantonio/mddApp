fde87e8794c88f8b819e5f042806833172387250
Matt Antonio <mantonio@fullsail.edu>
2014-04-23 18:17:26 -0400

    screwed up last pull and had to redo some previous changes

--- js/app/controllers/addFood_controller.js
+++ js/app/controllers/addFood_controller.js
@@ -4,10 +4,13 @@ app.controller("AddFoodController", function($scope) {
     $scope.$on('event:google-plus-signin-success', function(event, authResult) {
         // User successfully authorized the G+ App!
         console.log('Signed in!');
+        $("#addFood-btn").show();
+        $("#g-login").hide();
     });
     $scope.$on('event:google-plus-signin-failure', function(event, authResult) {
         // User has not authorized the G+ App!
         console.log('Not signed into Google Plus.');
+        $("#addFood-btn").hide();
     });
 
 });
---
+++ js/app/controllers/map_controller.js
@@ -0,0 +1,12 @@
+app.controller("MapController", function($scope, $http) {
+    $scope.googleMapsKey = "AIzaSyB0w2_dnlKE13DikhEpEO4MBUb7-ZtGFn4";
+
+    $scope.map = {
+        center: {
+            latitude: 28.597161,
+            longitude: -81.301508
+        },
+        zoom: 16
+    };
+
+});
