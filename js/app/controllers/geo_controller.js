08e32520a5538ed4fc5778764c0099a54c8f4db0
Matt Antonio <mantonio@fullsail.edu>
2014-05-01 13:58:54 -0400

    reformatted list view

--- js/app/controllers/geo_controller.js
+++ js/app/controllers/geo_controller.js
@@ -1,3 +1,13 @@
+/************************************************
+ *
+ * File - geo_controller.js
+ * Author - Matt Antonio
+ * Project - hungR
+ * Location - js/app/controllers/geo_controller.js
+ * Date - 04/15/14
+ *
+ ************************************************/
+
 app.controller("GeoController", function($rootScope, geolocation) {
     geolocation.getLocation().then(function(data) {
             
